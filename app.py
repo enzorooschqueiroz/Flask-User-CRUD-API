4b66583f8d2fff9ee7d3304fe007cee2012aa775
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-09-25 23:00:44 -0300

    :bug: fix : Assertion error

--- app.py
+++ app.py
@@ -4,22 +4,20 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
 class Users(Resource):
     def get(self):
         return {'message': 'user 1'}
-
 
 class User(Resource):
     def post(self):
         return {'message': 'teste'}
 
-        
     def get(self, cpf):
         return {'message': 'CPF 1'}
 
+# Definindo um endpoint explícito para evitar conflitos
+api.add_resource(Users, '/users', endpoint='users_list')  # Novo endpoint
+api.add_resource(User, '/user', '/user/<string:cpf>', endpoint='user_detail')
 
-api.add_resource(Users, '/users')
-api.add_resource(Users, '/user', 'user/<string:cpf>')
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
