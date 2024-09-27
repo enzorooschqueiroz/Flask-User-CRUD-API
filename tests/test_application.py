2d8b9c9be20cd77ecc159d726ad2edc776488197
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-09-27 14:54:01 -0300

    :test_tube: test: testing post user

--- tests/test_application.py
+++ tests/test_application.py
@@ -8,7 +8,37 @@ class TestApplication():
     def client(self): 
         app = create_app('config.MockConfig')
         return app.test_client()
+    
+    
+    @pytest.fixture
+    def valid_user(self):
+        return {
+            "cpf": "858.853.850-41",
+            "email": "user@example.com",
+            "first_name": "Gabriel Boiola",
+            "last_name": "Doe",
+            "birth_date": "1990-01-01"
+        }
+    
+    @pytest.fixture
+    def invalid_user(self):
+        return {
+            "cpf": "858.853.850-42",
+            "email": "user@example.com",
+            "first_name": "Gabriel Boiola",
+            "last_name": "Doe",
+            "birth_date": "1990-01-01"
+        }
 
     def test_get_users(self, client):
         response = client.get('/users')
         assert response.status_code == 200
+
+    def test_post_user(self, client, valid_user, invalid_user):
+        response = client.post('/user', json=valid_user)
+        assert response.status_code == 200
+        assert b"successfully" in response.data
+
+        response = client.post('/user', json=invalid_user)
+        assert response.status_code == 400 
+        assert b"Invalid" in response.data
